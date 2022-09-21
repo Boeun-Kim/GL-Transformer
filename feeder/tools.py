9f87b4a3f2182bf934a9ad691587846178b26ee9
Boeun-Kim <66453025+Boeun-Kim@users.noreply.github.com>
2022-09-21 16:26:01 +0900

    Update tools.py

--- feeder/tools.py
+++ feeder/tools.py
@@ -45,9 +45,9 @@ def interpolate(data_numpy, interpolate_ratio=0.0):
             tmp = cv2.resize(data_numpy[:, :, :, i].transpose(
                 [1, 2, 0]), (V, interpolate_size), interpolation=cv2.INTER_LINEAR)
 
-        tmp = tmp.transpose([2, 0, 1])
+            tmp = tmp.transpose([2, 0, 1])
 
-        new_data[:, :, :, i] = tmp
+            new_data[:, :, :, i] = tmp
 
     max_frame = 300
     if new_data.shape[1] > max_frame:
